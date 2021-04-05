22979e0aee6fc2d32d749fbec2c835bddfb2bfef
timurgordon <timurgordon@gmail.com>
2021-04-05 17:31:44 +0300

    added jsdoc

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './ProjectCard.css'
+import PropTypes from "prop-types"
 
-/* <ProjectsBar />
-* Takes in image, title, and location parameters.
-* Creates and returns project card with given parameters.
-*/
-export default function ProjectsBar(props) {
+/**
+ * Component for rendering a project card.
+ *
+ * @component
+ * @example
+ * const title = "Good Deed Project"
+ * const location = "New York, USA"
+ * const image = projectImg
+ * return (
+ *   <ProjectCard title={title} location={location} image={image} />
+ * )
+ */
+export default function ProjectCard(props) {
     return (
             <div className="card-container">
                 <img className="card-image" src={props.image}/>
@@ -14,3 +23,14 @@ export default function ProjectsBar(props) {
             </div>
     )
 }
+
+ProjectCard.propTypes = {
+    /**
+     * Project's title
+     */
+    title: PropTypes.string.isRequired,
+    /**
+     * Project's location
+     */
+    location: PropTypes.string.isRequired,
+  }
